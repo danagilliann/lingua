8ae9a9b77ecbac0f9cf5824ea0dffe7ddc62e167
danagilliann <danalee96@gmail.com>
2017-01-18 18:19:44 +0800

    css changes

--- src/client/app/InputComponent.jsx
+++ src/client/app/InputComponent.jsx
@@ -14,8 +14,12 @@ class InputComponent extends React.Component {
           Type in a word
         </h1>
         <div>
-          <input type='text' value='hello' name='word'/>
-          <input type='submit' value='submit'/>
+          <div className={index.inputWordDiv}>
+            <input className={index.inputWordText} type='text' value='Enter your word' name='word'></input>
+          </div>
+          <div className={index.inputSubmitDiv}>
+            <input className={index.inputSubmitDiv} type='submit' value='submit'/>
+          </div>
         </div>
       </div>
     );
