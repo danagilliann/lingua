db7520a230f838eddef9d9405c35b51d7745104d
danagilliann <danalee96@gmail.com>
2017-01-18 18:22:52 +0800

    states a workin

--- src/client/app/InputComponent.jsx
+++ src/client/app/InputComponent.jsx
@@ -5,17 +5,22 @@ class InputComponent extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {word: 'Enter your word'};
+  }
+
+  changeText(event) {
+    this.setState({value: event.target.value});
   }
 
   render() {
     return (
       <div>
-      <h1 className={[index.h1, index.title].join(' ')}>
-          Type in a word
+        <h1 className={[index.h1, index.title].join(' ')}>
+            Type in a word
         </h1>
         <div>
           <div className={index.inputWordDiv}>
-            <input className={index.inputWordText} type='text' value='Enter your word' name='word'></input>
+            <input className={index.inputWordText} type='text' value={this.state.word} onChange={this.changeText} name='word'></input>
           </div>
           <div className={index.inputSubmitDiv}>
             <input className={index.inputSubmitDiv} type='submit' value='submit'/>
